2db3405a4e6cb9ca9a11e804fbda0e5a4ea19afe
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2026-04-17 15:37:05 -0700

    feat(ui): enhance footer with navigation links, branding, and improved layout

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,4 +1,6 @@
 import "@/layout/Footer/Footer.css";
+import { Link } from "react-router";
+import logo from "@/assets/logo.svg";
 
 function Footer() {
   return (
@@ -18,6 +20,32 @@ function Footer() {
             <span>STAY IN THE LOOP</span>
             <span>STAY IN THE LOOP</span>
           </p>
+        </div>
+      </div>
+      <div className="footer-links">
+        <div className="column">
+          <Link to="/about" className="footer-link">About Us</Link>
+          <Link to="/projects" className="footer-link">Projects</Link>
+          <Link to="/members" className="footer-link">Members</Link>
+        </div>
+        <div className="column">
+          <Link to="/sponsors" className="footer-link">Sponsors</Link>
+          <Link to="" className="footer-link">Contact Us</Link>
+          <Link to="" className="footer-link">Apply Now</Link>
+        </div>
+        <div className="column">
+          <Link to="" className="footer-link">LinkedIn ↗</Link>
+          <Link to="" className="footer-link">Instagram ↗</Link>
+          <Link to="" className="footer-link">Facebook ↗</Link>
+          <Link to="" className="footer-link">Github ↗</Link>
+        </div>
+      </div>
+      <div className="footer-brand">
+        <img src={logo} alt="footer-logo" className="footer-logo" />
+        <div className="column">
+          <div className="footer-name">Bike</div>
+          <div className="footer-name">Builders</div>
+          <div className="footer-name">of Berkeley</div>
         </div>
       </div>
     </footer>
