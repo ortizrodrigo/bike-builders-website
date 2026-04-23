713c3e3903b1e1bc0d454daa0f2f0343a393152b
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2026-04-23 01:06:11 -0700

    feat(ui): integrate Carousel component into Sponsors section

--- src/pages/home/sections/sponsors/sponsors.tsx
+++ src/pages/home/sections/sponsors/sponsors.tsx
@@ -1,6 +1,8 @@
 import "@/pages/home/home.css";
 import "@/pages/home/sections/sponsors/sponsors.css";
 import { Link } from "react-router";
+import { Carousel } from "@/components";
+import placeholder from "@/assets/placeholder.jpeg";
 
 function SponsorsSection() {
   return (
@@ -10,7 +12,12 @@ function SponsorsSection() {
         We are grateful for the people and companies that make our journey possible!
       </p>
       {/* TODO: sponsor images inside Carousel Component*/}
-      <div className="sponsors-carousel">TODO: Sponsors Carousel</div>
+      <Carousel>
+        <img src={placeholder} alt="sponsor" className="sponsors-image" />
+        <img src={placeholder} alt="sponsor" className="sponsors-image" />
+        <img src={placeholder} alt="sponsor" className="sponsors-image" />
+        <img src={placeholder} alt="sponsor" className="sponsors-image" />
+      </Carousel>
       <Link className="blue-button" to={"/contact"}>
         Sponsor Us
       </Link>
